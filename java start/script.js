068d77cd5756684789bafe3b38043046821e5918
Mahi-Ali <164639420+Mahi-Ali@users.noreply.github.com>
2024-05-01 15:53:17 +0500

    Add files via upload

---
+++ java start/script.js
@@ -0,0 +1,31 @@
+// console.log("Hello World");
+// console.log("Mahik ALi");
+// document.write("Hello World");
+
+alert("Welcome to our website!");
+
+console.log("careful writer ends every declarative sentence with a period.");
+
+var first_name; //decliar
+first_name = 'Mahik';
+var first_name = 'Mahik';
+
+
+var first_name = "Mahik";
+console.log(first_name);
+let last_name = 'Ali';
+console.log(last_name);
+
+var number = 2021
+console.log(number);
+var text = "It is invented in 1845";
+console.log(text);
+var name = "Mahik Ali"
+var email = "Mahikroshanali@gmail.com";
+var dateofBirth= 2004;
+console.log(name);
+console.log(email);
+console.log(dateofBirth);
+ let a = 12;
+ let b = 16;
+ let result = a + b;
